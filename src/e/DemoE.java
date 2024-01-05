1b4e9c568f1d3c86672e2e00b3668fdf0a1404d2
pilar <pilar@DESKTOP-48AVSD1>
2024-01-04 21:08:52 -0700

    Commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,14 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Friday");
+		
+		System.out.println("Sprint 9 work...");
+		System.out.println("Sprint 10 work...");
+
+}
+
+}
